5ff96463de606b481b6eb3777addbb5895281dc3
Emilly <milu.silvaj@gmail.com>
2026-03-06 21:51:23 -0300

    Versao inicial

---
+++ Back/ayvy/src/main/java/br/belval/com/ayvy/AyvyApplication.java
@@ -0,0 +1,13 @@
+package br.belval.com.ayvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyvyApplication.class, args);
+	}
+
+}
---
+++ Back/ayvy/src/test/java/br/belval/com/ayvy/AyvyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.belval.com.ayvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
