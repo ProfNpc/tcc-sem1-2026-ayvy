dd7a7fedc07ac9db66c22964fd43c04be2c94902
GracieleSilva88 <rm101724@estudante.fieb.edu.br>
2026-05-10 22:30:12 -0300

    Ajuste tela de perfil do cliente

--- Ayvy TCC/perfil-cliente.js
+++ Ayvy TCC/perfil-cliente.js
@@ -1,16 +1,32 @@
-const uploadFoto = document.getElementById("uploadFoto");
-const fotoPerfil = document.getElementById("fotoPerfil");
+// perfil-cliente.js
 
-uploadFoto.addEventListener("change", function () {
-  const file = this.files[0];
+const uploadFoto =
+document.getElementById('uploadFoto');
 
-  if (file) {
-    const reader = new FileReader();
+const fotoPerfil =
+document.getElementById('fotoPerfil');
 
-    reader.onload = function (e) {
-      fotoPerfil.src = e.target.result;
-    };
+uploadFoto.addEventListener(
+  'change',
+  function () {
 
-    reader.readAsDataURL(file);
+    const arquivo = this.files[0];
+
+    if (arquivo) {
+
+      const leitor =
+      new FileReader();
+
+      leitor.onload = function (e) {
+
+        fotoPerfil.src =
+        e.target.result;
+
+      };
+
+      leitor.readAsDataURL(arquivo);
+
+    }
+
   }
-});
+);
